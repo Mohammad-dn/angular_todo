a1df5ccc9b532c00cef88c8d7ffe44a0dbc116bb
Mohammad-dn <mohammaddehghani2677@gmail.com>
2025-02-05 17:04:10 +0330

    implement layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
 	selector: "app-root",
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, HeaderComponent, FooterComponent],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
 	title = "todo-app";
+	name = "Mohammad";
+	handleClick() {
+		alert(`hello ${this.name}`);
+	}
+	handleMouseEnter(e: MouseEvent) {
+		console.log(e);
+	}
 }
