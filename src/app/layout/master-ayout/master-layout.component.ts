d7a77fd52f80bd1550bf26bf9d55840dcf1ea4fc
Mohammad-dn <mohammaddehghani2677@gmail.com>
2025-02-11 15:35:16 +0330

    fix

--- src/app/layout/master-ayout/master-layout.component.ts
+++ src/app/layout/master-ayout/master-layout.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { RouterOutlet } from "@angular/router";
-
 @Component({
 	selector: "app-master-layout",
 	templateUrl: "./master-layout.component.html",
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [
+		RouterOutlet,
+		MatSidenavModule,
+		MatToolbarModule,
+		MatButtonModule,
+		MatIconModule,
+	],
+	styleUrl: "./master-layout.component.css",
 })
 export class MasterLayoutComponent implements OnInit {
 	constructor() {}
+	showFiller = false;
 
 	ngOnInit(): void {}
 }
